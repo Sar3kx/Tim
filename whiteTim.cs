350a0c61ca5fddc3439e11d5aaf7639f73aaf646
Sar3kx <sar3kx@gmail.com>
2016-10-27 20:49:58 +0530

    iOS Popular Game Phase   .. Player Codes C#

--- whiteTim.cs
+++ whiteTim.cs
@@ -4,28 +4,23 @@ using System.Collections;
 public class whiteTim : MonoBehaviour {
 
 	//floats
-
 	public float moveSpeed;
-	public float ball_xvel, ball_yvel;
+	private float ball_xvel, ball_yvel;
+	float moveVelocity;
 
 	private Vector2 movement;
-
 	public LayerMask playerLayerMask;
 
 
 	//Booleans
-	private bool IsDead;
+	public bool IsDead;
 	public bool isGrounded;
 
 
 
-	//Animators
-	public Animator WhiteGroundShake;
-
-
 	//Otherscripts
 	public SmoothCamera2D cam;
-	public Buttons Button;
+
 
 
 	//Gameobjects
@@ -34,7 +29,7 @@ public class whiteTim : MonoBehaviour {
 
 
 	//AudioClips
-	public AudioClip BallHIT;
+	//public AudioClip BallHIT;
 
 
 	//Transforms
@@ -45,84 +40,80 @@ public class whiteTim : MonoBehaviour {
 	//Rigidbody2D
 	private Rigidbody2D myBody;
 
+    //animator
+    public Animator PlayerDiedPanal;
 
-	// Use this for initialization
-	void Start () {
+
+
+    void Start ()
+    {
 		myTrans = this.transform;
 		myBody = this.GetComponent<Rigidbody2D> ();
 		PlayerTim = this.gameObject;
-		//tagGround = GameObject.Find(this.name +"/tag_Ground).transform;
 
-
-	
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	
+	void Update ()
+    {
 		isGrounded = 
-			Physics2D.Linecast (myTrans.position, tagGround.transform.position, playerLayerMask) || 
-			Physics2D.Linecast (myTrans.position, tagGround1.transform.position, playerLayerMask) || 
-			Physics2D.Linecast (myTrans.position, tagGround2.transform.position, playerLayerMask) || 
-			Physics2D.Linecast (myTrans.position, tagGround3.transform.position, playerLayerMask) ||
-			Physics2D.Linecast (myTrans.position, tagGround4.transform.position, playerLayerMask) ;  
-		/*	Debug.DrawLine (myTrans.position,GameObject.Find(this.name+"/tag_ground3").transform.position,Color.red);
-			Debug.DrawLine (myTrans.position,GameObject.Find(this.name+"/tag_ground4").transform.position,Color.red);
-			ebug.DrawLine (myTrans.position,GameObject.Find(this.name+"/tag_ground2").transform.position,Color.red);*/
+		Physics2D.Linecast (myTrans.position, tagGround.transform.position, playerLayerMask) ||
+		Physics2D.Linecast (myTrans.position, tagGround1.transform.position, playerLayerMask) ||
+		Physics2D.Linecast (myTrans.position, tagGround2.transform.position, playerLayerMask) ||
+		Physics2D.Linecast (myTrans.position, tagGround3.transform.position, playerLayerMask) ||
+		Physics2D.Linecast (myTrans.position, tagGround4.transform.position, playerLayerMask); 
+		//	Debug.DrawLine (myTrans.position,tagGround.transform.position,Color.red);
 
 
 
-
+		moveVelocity = 0;
 		//Input 
-		if (Input.GetKey  (KeyCode.Mouse0)) {
-			myBody.velocity = transform.right * moveSpeed;
-			//transform.Translate (Vector2.right * moveSpeed * Time.fixedTime);
-			//transform.Translate(transform.right * moveSpeed* Time.smoothDeltaTime );
+		if (Input.GetKey (KeyCode.Mouse0))
+        {	
+			moveVelocity = moveSpeed;
+		}
+		if (Input.GetKey (KeyCode.Mouse1))
+        { 	
+			moveVelocity = -moveSpeed;
 		}
 
-		if (Input.GetKey   (KeyCode.Mouse1)) {
-			//myBody.AddForce (-Vector2.right * moveSpeed * Time.deltaTime);
-			//transform.Translate(-transform.right * moveSpeed* Time.smoothDeltaTime );
-		}
-		if (isGrounded) {
-			
-			ball_yvel = 10;
-			gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, ball_yvel);
-			//myBody.velocity += jumpForce * Vector2.up * Time.deltaTime;
 
+		myBody.velocity = new Vector2 (moveVelocity, myBody.velocity.y);
+			if (isGrounded)
+        {
+   			ball_yvel = 16;
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(ball_xvel, ball_yvel);
+            
+        }
+
+		if (IsDead == true)
+        {
+			PlayerTim.SetActive (false);
+			cam.shakeCamera (0.1f, 1);
+			Instantiate(Shatter, PlayerTim.transform.position, Quaternion.Euler(0, 180, 0));
+            PlayerDiedPanal.SetTrigger("PlayerdiedPanalHIT");
 		}
 	}
-
-
-
-
-
-
-
+	
 
 	//Collision Enter 
 	public void OnCollisionEnter2D(Collision2D col)
 	{
-
-		//isGrounded = true;
 		if (col.gameObject.tag == "BlackGround")
 		{
 			IsDead = true;
-			//Destroy(gameObject);
-			PlayerTim.SetActive (false);
-			cam.shakeCamera(0.1f, 1);
-			//  Instantiate(playerDeadPartical, PlayerTim.transform.position, Quaternion.Euler(0, 180, 0));
+		}
+		if (col.gameObject.tag == "Enemy") {
+			IsDead = true;
 		}
 
-		if (col.gameObject.tag == "whiteShakingGround")
-		{
-			WhiteGroundShake.SetTrigger("PlayerHIT");
 
-		}
+		
 	}
 
 	//Collision Exit
-	public void OnCollisionExit2D(Collision2D col){
+	public void OnCollisionExit2D(Collision2D col)
+    {
 		//isGrounded = false;
 	}
 
@@ -130,13 +121,29 @@ public class whiteTim : MonoBehaviour {
 	//Trigger Enter 
 	public void OnTriggerEnter2D(Collider2D Other)
 	{
+		if (Other.gameObject.tag == "TempSupporter")
+        { 
+			Other.gameObject.SetActive (false);
+			cam.shakeCamera (0.1f, 10f);
+		}
+
+        if (Other.gameObject.tag == "BlackGround")
+        {
+            IsDead = true;
+        }
+        if (Other.gameObject.tag == "Enemy")
+        {
+            IsDead = true;
+        }
+
+    }
+
+	public void OnTriggerExit2D(Collider2D Other)
+    {
 
 
 
 
 	}
-
-
-
-
+    
 }
